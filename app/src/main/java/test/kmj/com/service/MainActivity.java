34c9abacb97320a8e0e49d0ba7f7e6feb9f50238
minjun3021 <minjun3021@naver.com>
2018-12-23 02:18:02 +0900

    2st

--- app/src/main/java/test/kmj/com/service/MainActivity.java
+++ app/src/main/java/test/kmj/com/service/MainActivity.java
@@ -43,4 +43,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
+
+
 }
--- app/src/main/java/test/kmj/com/service/MyService.java
+++ app/src/main/java/test/kmj/com/service/MyService.java
@@ -4,10 +4,17 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
+
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import app.akexorcist.bluetotohspp.library.BluetoothSPP;
+import app.akexorcist.bluetotohspp.library.BluetoothState;
+
 public class MyService extends Service {
+    Boolean exit=true;
+    Boolean Created=false;
+    BluetoothSPP bt=new BluetoothSPP(this);;
 
     IBinder mBinder = new MyBinder();
 
@@ -31,6 +38,11 @@ public class MyService extends Service {
     public void onCreate() {
         super.onCreate();
         // 서비스에서 가장 먼저 호출됨(최초에 한번만)
+
+        blueToothStart();
+
+
+
         Log.e("test", "서비스의 onCreate");
 
     }
@@ -38,7 +50,7 @@ public class MyService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId){
         Log.e("test", "서비스의 onStartCommand");
 
-
+        blueToothStart();
 
 
         return super.onStartCommand(intent, flags, startId);
@@ -48,8 +60,51 @@ public class MyService extends Service {
     public void onDestroy() {
         super.onDestroy();
         // 서비스가 종료될 때 실행
-
+        bt.stopService();
         Log.e("test", "서비스의 onDestroy");
+    }
+
+    void blueToothStart(){
+        if (!bt.isBluetoothEnabled()) {
+            bt.enable();
+        } else {
+            if (!bt.isServiceAvailable()) {
+                bt.setupService();
+                bt.startService(BluetoothState.DEVICE_OTHER);
+                bt.autoConnect("HC-06");
+            }
+        }
+
+
+        bt.setOnDataReceivedListener(new BluetoothSPP.OnDataReceivedListener() {
+            public void onDataReceived(byte[] data, String message) {
+                //ㄷㅔ이터 받아질때 매번 실행
+                Log.e("test", message+" "+bt.getConnectedDeviceName());
+
+            }
+        });
+
+        bt.setBluetoothConnectionListener(new BluetoothSPP.BluetoothConnectionListener()
+
+        {
+            public void onDeviceConnected(String name, String address) {
+
+                //연결됐을때
+                Log.e("test", name+address);
+                Log.e("test", "connect");
+
+            }
+
+            public void onDeviceDisconnected() {    //연결끊김
+                Log.e("test", "consad");
+
+            }
+
+            public void onDeviceConnectionFailed() {
+                //연결 실패했을때
+                Log.e("test", "sad");
+            }
+        });
     }
 
 }
